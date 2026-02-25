30e908589729013912d5c17e0f048cc23be88a74
Louise Aldridge <louisealdridge88@gmail.com>
2026-02-25 11:04:35 +0000

    Creating and rendering header component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
-<script setup></script>
+<script setup>
+import Header from "@/components/Header.vue";
+</script>
 
 <template>
-  <h1>Hello</h1>
+  <div class="wrapper">
+    <Header></Header>
+  </div>
 </template>
 
 <style scoped></style>
