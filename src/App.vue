a92fffca3f1f5314e8f8b5f1e3c69e5a4fc338a1
Louise Aldridge <louisealdridge88@gmail.com>
2026-02-25 14:54:04 +0000

    Creating and using vue router for views

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
+import { RouterView } from "vue-router";
 import Header from "@/components/Header.vue";
+import Navigation from "@/components/Navigation.vue";
 </script>
 
 <template>
   <div class="wrapper">
     <Header></Header>
+    <Navigation></Navigation>
+    <RouterView></RouterView>
   </div>
 </template>
 
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,10 @@
-<script setup></script>
-<template></template>
+<script setup>
+import { RouterLink } from "vue-router";
+</script>
+<template>
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/counter">Counter</RouterLink>
+  </nav>
+</template>
 <style scoped></style>
